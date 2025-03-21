8a790cc3abf10a28c4c8a3ed6e6adcaa5c74f038
André Teixeira <andre-x8@hotmail.com>
2025-03-20 22:24:50 -0300

    implementa cmpomente de filtro

--- src/app/angular-material/angular-material.module.ts
+++ src/app/angular-material/angular-material.module.ts
@@ -1,10 +1,35 @@
 import { NgModule } from '@angular/core';
-import { MatButtonModule } from '@angular/material/button';
-import { MatDividerModule } from '@angular/material/divider';
 import { MatListModule } from '@angular/material/list';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDatepickerModule } from '@angular/material/datepicker';
 
 @NgModule({
-  imports: [MatButtonModule, MatListModule, MatDividerModule],
-  exports: [MatButtonModule, MatListModule, MatDividerModule],
+  imports: [
+    MatButtonModule,
+    MatListModule,
+    MatDividerModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatSelectModule,
+    MatButtonModule,
+  ],
+  exports: [
+    MatButtonModule,
+    MatListModule,
+    MatDividerModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatSelectModule,
+    MatButtonModule,
+  ],
 })
 export class AngulaMaterialModule {}
--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { AngulaMaterialModule } from '../angular-material/angular-material.module';
 import { UserDetailsComponent } from './user-details/user-details.component';
 import { FilterComponent } from './filter/filter.component';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [UserDetailsComponent, FilterComponent],
-  imports: [AngulaMaterialModule],
-  exports: [UserDetailsComponent],
+  imports: [AngulaMaterialModule, FormsModule, CommonModule],
+  exports: [UserDetailsComponent, FilterComponent],
 })
 export class ComponentsModule {}
